ef819f8705e2e1a91a3be0b358a2a6d9c83ff213
thisisway185 <90272458+thisisway185@users.noreply.github.com>
2021-10-07 23:38:25 +0300

    summary
    no .

---
+++ main.py
@@ -0,0 +1,20 @@
+def rangee(x=1, y=10):
+    """This program makes the range function more interesting."""
+    print("Welcome to the program.")
+    answer = True
+    while answer:
+       x = int(input("\nWrite the first index: "))
+       y = int(input("Write the second index: "))
+       while x<y:
+           x += 1
+           print(x)
+       answer = input("Do you use rangee again? yes/no: ")
+       if answer=="yes":
+           continue
+       elif answer =="no":
+           print("Thank you for using our program.")
+           answer = False
+       else:
+            print("No such command was found, the program started from scratch.")
+       return x
+rangee()
